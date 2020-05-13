be56878659de070c8e6fd38125493a6ebf7fec23
laoxu80 <xuli.21@163.com>
2020-05-13 17:37:23 +0800

    20200513 change url

--- algo/web_ex.py
+++ algo/web_ex.py
@@ -3,20 +3,24 @@ import time
 
 
 website = webdriver.Chrome()
-dgl_url= "https://www.daguanlian.com"
+dgl_url= "https://admin.daguanlian.com"
 test_url = "http://bbs.51testing.com"
-website.get(test_url)
-
+website.get(dgl_url)
+"""
 sea=website.find_element_by_id("scbar_txt")
 sea.send_keys("jemter")
 sub=website.find_element_by_id("scbar_btn")
+
+
 sub.click()
+
+
 
 time.sleep(3)
 website.close()
 time.sleep(1)
 website.quit()
-
+"""
 
 
 

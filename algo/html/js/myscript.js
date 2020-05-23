bdc845ad4db1778e14a5430d9b71dff7add2ca75
laoxu80 <xuli.21@163.com>
2020-05-23 17:17:56 +0800

    20200523js

--- algo/html/js/myscript.js
+++ algo/html/js/myscript.js
@@ -2,3 +2,19 @@ function myFunction(id)
 {
     document.getElementById(id).innerHTML="我的第一个 JavaScript 函数";
 }
+
+function login(a,b)
+{   
+    var num_a,num_b
+    num_a=Number(document.getElementById(a).value)
+    num_b=Number(document.getElementById(b).value)
+    if(Number(document.getElementById("login").value)==num_a+num_b)
+    {
+        alert("right")
+    }
+    else{
+        alert("wrong")
+    }
+
+    //console.log(document.getElementById(a).value)
+}

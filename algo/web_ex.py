da83d056a33766fcf8031ad8d409510ff038fd72
laoxu80 <xuli.21@163.com>
2020-05-26 20:56:05 +0800

    20200526 增加委托单管理操作

--- algo/web_ex.py
+++ algo/web_ex.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # 打开网页
 website = webdriver.Chrome()
@@ -12,12 +15,42 @@ website.maximize_window()
 login_ele = website.find_element_by_xpath("//a[@class='fl wd_fff fs12 pointer']")
 login_ele.click()
 tel_ele = website.find_element_by_name("name")
-tel_ele.send_keys("12000000004")
+tel_ele.send_keys("11000000004")
 pwd_ele = website.find_element_by_name("password")
 pwd_ele.send_keys("111111")
 com_ele = website.find_element_by_xpath("//button[@class='login_btn userLogin01']")
 com_ele.click()
+time.sleep(2)
+# 验证登录用户正确
+#username = website.find_element_by_id("userName")
+"""
 # 货源市场点击发布货源信息
+website.find_element_by_xpath("//li[@class='ng-scope menuL']/a[@class='inactive']/p[contains(text(),'货源市场')]").click()
+time.sleep(2)
+#website.find_element_by_xpath("//a[@class='ru-a menu-a']/p[text()='货源发布']").click()
+#website.find_element_by_xpath("//img[@src='../static/images/hyfb.png']").click()
+
+pub_ele = website.find_element_by_xpath("//a[@class='menu_son_act']")
+print(pub_ele)
+"""
+# 委托单管理
+website.find_element_by_xpath("//li[@class='ng-scope menuL']/a[@class='inactive']/p[contains(text(),'委托单管理')]").click()
+time.sleep(1)
+website.find_element_by_xpath("//span[contains(text(),'委托单管理')]").click()
+time.sleep(20)
+
+# 新增委托单
+#neworder_ele =WebDriverWait(website,10,1).until(EC.presence_of_element_located((By.XPATH,"//button[text()='新增委托单']")))
+website.find_element_by_xpath("//div[@class='fl clearfix mt8 pl10']/button[text()='新增委托单']").click()
+#neworder_ele.click()
+
+
+
+
+
+
+
+
 
 
 
